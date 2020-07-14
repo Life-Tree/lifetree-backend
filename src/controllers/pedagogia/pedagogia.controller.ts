c85ba779aef39ae6aa33ea06f6e1f56532b6fdfb
@deveant <cermanthony@gmail.com>
2020-07-14 02:14:10 -0500

    nombres de los metodos del controlador pedagogia

--- src/controllers/pedagogia/pedagogia.controller.ts
+++ src/controllers/pedagogia/pedagogia.controller.ts
@@ -11,23 +11,23 @@ export class PedagogiaController {
     }
 
     @Get('/:idMP')
-    getArbol(@Param('idMP') idMP: string): Promise<MaterialPedagogico>{
+    getPedagogia(@Param('idMP') idMP: string): Promise<MaterialPedagogico>{
         return this.pedagogiaManager.getMP(idMP);
     }
 
     @Get()
-    getArboles(): Promise<MaterialPedagogico[]>{
+    getPedagogias(): Promise<MaterialPedagogico[]>{
         return this.pedagogiaManager.getMPs();
     }    
 
     @Post()
-    async crearArbol(@Body() mPedagogico: PedagogiaDTO): Promise<boolean>{
+    async crearPedagogia(@Body() mPedagogico: PedagogiaDTO): Promise<boolean>{
         let result = await this.pedagogiaManager.nuevoMP(mPedagogico.titulo,mPedagogico.descripcion,mPedagogico.dataURL);
         return result;
     }
 
     @Put(':idMP')
-    modificarArbol(@Body() mPedagogico: PedagogiaDTO, @Param('idMP') idMP: string): string{
+    modificarPedagogia(@Body() mPedagogico: PedagogiaDTO, @Param('idMP') idMP: string): string{
         let result = this.pedagogiaManager.updateMP(idMP,mPedagogico.titulo,mPedagogico.descripcion,mPedagogico.dataURL);        
         return `Material Pedagógico actualizado?: ${result}`;
     }
